2b3f13b25c225809921c14908252ec6cceedd03c
LetoCode <pavolov.egor@gmail.com>
2022-12-22 12:45:51 +0300

    feat: add some more

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -114,8 +114,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    let res;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (err) {
+        res = attempts;
+      }
+    }
+    return res;
+  };
 }
 
 
@@ -142,8 +152,14 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const mess = `${func.name}(${JSON.stringify(args).slice(1, -1)})`;
+    logFunc(`${mess} starts`);
+    const res = func(...args);
+    logFunc(`${mess} ends`);
+    return res;
+  };
 }
 
 
